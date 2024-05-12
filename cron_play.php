1714a760322312b6bf9e4103d2bd5a9de74e64b1
Vadim Babajanyan <akeb@akeb.ru>
2024-05-12 11:03:13 +0300

    Update cron_play.php

--- cron_play.php
+++ cron_play.php
@@ -1,9 +1,12 @@
 <?php
 require_once('config.php');
 require_once('functions.php');
+$startTime = time();
 
+addToLog("Start");
 if ($fp = fopen(constant('DB_FILE_NAME'), 'r+')) {
 	// Lock File
+	addToLog("lock file");
 	do {
 		$canWrite = flock($fp, LOCK_EX);
 		// If lock not obtained sleep for 0 - 100 milliseconds, to avoid collision and CPU load
@@ -11,13 +14,13 @@ if ($fp = fopen(constant('DB_FILE_NAME'), 'r+')) {
 	} while ((!$canWrite) && ((microtime(TRUE)-$startTime) < 5));
 
 	if ($canWrite) {
+		addToLog("canWrite");
 		$alarms = read_database();
 
 		$week = intval(date("w")) - 1;
 		$hour = intval(date("G"));
 		$minute = intval(date("i"));
 
-		// addToLog('Start');
 		foreach($alarms as $k=>$alarm) {
 			if (!$alarm['status']) continue;
 			// addToLog('1 '.var_export($alarm, true));
@@ -39,12 +42,12 @@ if ($fp = fopen(constant('DB_FILE_NAME'), 'r+')) {
 				$alarms[$k]['status'] = false;
 				write_database($alarms, true);
 			}
-
+   addToLog("play sound");
 			exec('nohup ./play.sh sounds/'. $alarm['sound'] . '.mp3 '.intval($alarm['volume']).' &> /dev/null & ');
 
 			break;
 		}
-
+  addToLog("unlock file");
 		flock($fp, LOCK_UN);
 	}
 	fclose($fp);
@@ -52,4 +55,4 @@ if ($fp = fopen(constant('DB_FILE_NAME'), 'r+')) {
 
 safe_file_rewrite('get_database_hash.txt', md5_file(constant('DB_FILE_NAME')));
 
-// addToLog('Finish');
+addToLog('Finish');
